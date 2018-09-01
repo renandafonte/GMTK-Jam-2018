d787c5f6e5845c9f7de549b2dd360c3756a72bb5
LAPTOP-UTJNBTG5\gusta <gustbm78@gmail.com>
2018-09-01 15:57:42 -0300

    Mais testes

--- GameJam-GMTK-2018/Assets/Scripts/Text/InkTextObject.cs
+++ GameJam-GMTK-2018/Assets/Scripts/Text/InkTextObject.cs
@@ -6,8 +6,25 @@ using Ink.Runtime;
 public class InkTextObject : MonoBehaviour {
 	public TextAsset JsonFromInk;
 	private Story _story;
+    List<string> myTagString;
+    private AudioSource audioVoice;
 
-	public void CallText(){
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        /*string myText = _story.Continue();
+        List<string> myTagString = _story.currentTags;
+        string speakerAudioLine = myTagString[0];
+
+        if (Input.GetKeyDown("space"))
+            Debug.Log(speakerAudioLine);*/
+    }
+
+    public void CallText(){
 		//Inicia classe story (veio do plugin do ink)
 		_story = new Story(JsonFromInk.text);
 		TextManager.instance.SetTextActive(true); //ativa a caixa de texto
@@ -18,7 +35,11 @@ public class InkTextObject : MonoBehaviour {
 		while(_story.canContinue){
 			string text = _story.Continue(); //continua história enquanto possível
 			TextManager.instance.UpdateText(text); //muda texto
-		}
+            myTagString = _story.currentTags;
+            Debug.Log(_story.currentTags);
+            Debug.Log(myTagString[0]);
+
+        }
 	}
 
 	public void ChooseChoice(int index){
@@ -36,4 +57,10 @@ public class InkTextObject : MonoBehaviour {
 				break;
 		}
 	}
+
+    public void tocaPlay(string audioNom)
+    {
+        AudioSource audio = GetComponent<AudioSource>();
+        audio.Play();
+    }
 }
