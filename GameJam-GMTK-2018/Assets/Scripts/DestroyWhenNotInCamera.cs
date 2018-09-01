10a72dbeeac3d303c185d033fe6024475b2797fe
Renan <renandafonte@gmail.com>
2018-09-01 16:05:17 -0300

    component to make object invisible when out of camera

---
+++ GameJam-GMTK-2018/Assets/Scripts/DestroyWhenNotInCamera.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyWhenNotInCamera : MonoBehaviour {
+	// Update is called once per frame
+	void OnBecameInvisible(){
+		gameObject.SetActive(false);
+	}
+}
