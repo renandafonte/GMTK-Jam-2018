fa0d4ad1e7791a05b1f5ed513db1722e54fb01b3
LAPTOP-UTJNBTG5\gusta <gustbm78@gmail.com>
2018-09-01 13:53:15 -0300

    no message

---
+++ GameJam-GMTK-2018/Assets/Scripts/CameraControl.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour {
+
+	public enum RotationAxis
+    {
+        MouseX = 1,
+        MouseY = 2
+    }
+
+    public RotationAxis axes = RotationAxis.MouseX;
+    public float sensHorizontal = 10.0f;
+    public float sensVertical = 10.0f;
+
+    public float _rotationX = 0;
+
+    private void Update()
+    {
+        if(axes == RotationAxis.MouseX)
+        {
+            transform.Rotate(0, Input.GetAxis("Mouse X") * sensHorizontal, 0);
+        }else if(axes == RotationAxis.MouseY)
+        {
+            _rotationX -= Input.GetAxis("Mouse Y") * sensVertical;
+        }
+    }
+}
