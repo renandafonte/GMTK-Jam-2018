8999fbd785d538f9e496791dddb5f2f57ec3d145
Renan <renandafonte@gmail.com>
2018-09-01 16:08:28 -0300

    removendo comentário do update

--- GameJam-GMTK-2018/Assets/Scripts/DestroyWhenNotInCamera.cs
+++ GameJam-GMTK-2018/Assets/Scripts/DestroyWhenNotInCamera.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class DestroyWhenNotInCamera : MonoBehaviour {
-	// Update is called once per frame
 	void OnBecameInvisible(){
 		gameObject.SetActive(false);
 	}
