d613976719ecdadec5f45c3b7d948633f18ff7c9
Renan <renandafonte@gmail.com>
2018-09-01 22:00:57 -0300

    bg music

---
+++ GameJam-GMTK-2018/Assets/Scripts/Audio/BGMusicManager.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGMusicManager : MonoBehaviour {
+	public static BGMusicManager instance;
+	void Awake(){
+		instance = this;
+		DontDestroyOnLoad(gameObject);
+	}
+}
